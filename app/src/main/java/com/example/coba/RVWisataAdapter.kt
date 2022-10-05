9ec49b320e3b62b7fb1b05ab1afdae21b3d277d3
TheoDorus <theodorusadiyatma96@gmail.com>
2022-10-06 00:23:03 +0700

    Menambahkan data binding pada RV Wisata

--- app/src/main/java/com/example/coba/RVWisataAdapter.kt
+++ app/src/main/java/com/example/coba/RVWisataAdapter.kt
@@ -7,19 +7,22 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.coba.databinding.RvWisataBinding
 import com.example.coba.room.Wisata
 
 class RVWisataAdapter(private val data: ArrayList<Wisata>, private var listener: onAdapterListener) : RecyclerView.Adapter<RVWisataAdapter.viewHolder>() {
-
+    private lateinit var binding: RvWisataBinding
     override fun onCreateViewHolder(parent: ViewGroup,viewType: Int): viewHolder{
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.rv_wisata, parent, false)
-        return viewHolder(itemView)
+        binding = RvWisataBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return viewHolder(binding)
     }
+
     override fun onBindViewHolder(holder: viewHolder,position: Int) {
         val currentItem = data[position]
-        holder.tvNamaWisata.text = currentItem.nama
-        holder.tvLokasi.text = currentItem.lokasi
-        holder.tvImageWisata.setImageResource(R.drawable.image_wakatobi)
+        holder.bind(currentItem)
+//        holder.tvNamaWisata.text = currentItem.nama
+//        holder.tvLokasi.text = currentItem.lokasi
+//        holder.tvImageWisata.setImageResource(R.drawable.image_wakatobi)
 
         holder.icon_edit.setOnClickListener{
             listener.onUpdate(currentItem)
@@ -38,10 +41,13 @@ class RVWisataAdapter(private val data: ArrayList<Wisata>, private var listener:
         return data.size
     }
 
-    class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val tvNamaWisata : TextView = itemView.findViewById(R.id.tv_nama_wisata)
-        val tvLokasi : TextView = itemView.findViewById(R.id.tv_lokasi_wisata)
-        val tvImageWisata : ImageView = itemView.findViewById(R.id.imageWisata)
+    class viewHolder(val binding: RvWisataBinding) : RecyclerView.ViewHolder(binding.root){
+//        val tvNamaWisata : TextView = itemView.findViewById(R.id.tv_nama_wisata)
+//        val tvLokasi : TextView = itemView.findViewById(R.id.tv_lokasi_wisata)
+//        val tvImageWisata : ImageView = itemView.findViewById(R.id.imageWisata)
+        fun bind(wisata: Wisata){
+            binding.wisata = wisata
+        }
         val icon_edit : ImageView = itemView.findViewById(R.id.icon_edit)
         val icon_delete : ImageView = itemView.findViewById(R.id.icon_delete)
     }
