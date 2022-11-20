f1111cadd7bb10ada531ddf61d76edcb36d058e6
TheoDorus <theodorusadiyatma96@gmail.com>
2022-11-20 16:38:25 +0700

    Nambah Beberapa package, class, dan vector

---
+++ app/src/main/java/com/example/coba/models/User.kt
@@ -0,0 +1,5 @@
+package com.example.coba.models
+
+class User (var nama: String,var borndate: String,var email: String,var phoneNum: String,var username: String,var password: String) {
+    var id : Long? = null
+}
